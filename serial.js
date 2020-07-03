969a3ea8909e20d9875bfc25cef66eabcec6ad95
bmatusiak <bmatusiak@gmail.com>
2020-07-03 16:08:25 -0400

    update

--- serial.js
+++ serial.js
@@ -10,42 +10,142 @@ bytes2string = function bytes2string(bytes) {
 
 wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-var hids = nodeHID.devices();
-
 //console.log(hids)
 
-var hid = false;
+var $hids = {};
 
-for(var i in hids){
-  if(hids[i].product =="ONLYKEY" && hids[i].interface == 3){
-    hid = hids[i];
-  }
+
+/*
+0: black
+1: red
+2: green
+3: yellow
+4: blue
+5: magenta
+6: cyan
+7: white
+*/
+
+var colorRGB = function(c){return '\x1b[38;2;'+c+'m';}
+
+var colorSwatch = {
+    "red": "255;0;0",
+    "orange": "255;165;0",
+    "yellow": "255;255;0",
+    "green": "50;205;50",
+    "teal": "0;128;128",
+    "blue": "0;0;255",
+    "purple": "128;0;128",
+    "white": "255;255;255",
+};
+var color = {};
+for (var i in colorSwatch){
+	color[i] = colorRGB(colorSwatch[i])
 }
 
 
+//process.stdout.write('Hello from '+color.red+'term.js'+color.white+' $ ');
 
-if(hid){
-	//console.log(hids)
-	var hidDev = new nodeHID.HID(hid.path);
+function findInterface(){
+	var hids = nodeHID.devices();
+	var isDev = 2;//not dev interface
+	
+	for(var i in hids){
+		if(hids[i].product =="ONLYKEY"){
+			if(hids[i].interface == 3){
+				isDev = 3;
+			}
+		}
+	}
+	return isDev;	
+}
 
-	//console.log("SELECTED",hidDev.getDeviceInfo(),hid);
-	// hidDev.setNonBlocking(1)
-	// hidDev.read(console.log);
-	// var out
-	// while(true){
-	//     hidDev.read
-	// }
+function findHID(hid_interface){
+	var hids = nodeHID.devices();
+
+	if(!$hids[hid_interface])
+		$hids[hid_interface] = {}
+	
+	$hids[hid_interface].finding = true;
+	
+	if($hids[hid_interface].com){
+		$hids[hid_interface].com = false;
+		process.stdout.write(color.yellow+"Disconnected onlykey interface "+hid_interface+"\r\n"+color.white)
+	}
+
+	for(var i in hids){
+		if(hids[i].product =="ONLYKEY"){
+			if(hids[i].interface == hid_interface){
+				$hids[hid_interface].com = false;
+				$hids[hid_interface].device = hids[i];
+			}
+		}
+	}	
+
+	if(!$hids[hid_interface].com && $hids[hid_interface].device){
+		try{
+			$hids[hid_interface].com = new nodeHID.HID($hids[hid_interface].device.path);
+			process.stdout.write(color.yellow+"Connected onlykey interface "+hid_interface+"\r\n"+color.white)
+		}catch(e){}
+	}
+	
+	$hids[hid_interface].finding = false;
+}
 
 
-
-	setInterval(function(){
-
-		var bfrstr = bytes2string(hidDev.readSync());
-		process.stdout.write(bfrstr)
+function readHID(hid_interface, $color, addNewLines){
+		if(!$hids[hid_interface]) 
+			return findHID(hid_interface);
 		
-	},1)		
-}else{
-	console.log(hids)
+		if($hids[hid_interface] && $hids[hid_interface].finding) 
+			return;
+		
+		if($hids[hid_interface] && $hids[hid_interface].com){
+			try{
+				var bfrstr = bytes2string($hids[hid_interface].com.readSync());
+				if(addNewLines){
+					process.stdout.write("\r\n")
+				}
+				process.stdout.write($color);
+				process.stdout.write(bfrstr);
+				process.stdout.write(color.white);
+				if(addNewLines){
+					process.stdout.write("\r\n")
+				}
+			}catch(e){
+				//console.log(e)
+				findHID(hid_interface);
+			}
+			return;
+		}
+		findHID(hid_interface);
 }
 
-//console.log(bytes2string(hidDev.readSync()));
+
+var looping = false;
+
+setInterval(function(){
+
+	try{
+		var $interface = findInterface();
+		readHID($interface, $interface == 2 ? color.red : color.white, $interface == 2 ? true : false )
+	}catch(e){}
+	
+
+/*
+	try{
+		readHID(2, color.red,true)
+	}catch(e){}
+*/	
+	
+//		console.log(e)
+//		findHID(3);
+	
+	/*
+	try{
+		readHID(2, color.red, true)
+	}catch(e){
+		findHID(2);
+	}
+	*/
+},1)		
