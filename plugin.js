46071f4c8c88834e2be796d40bfb05061aa5116e
Ubuntu <ubuntu@ip-172-31-55-192.ec2.internal>
2020-07-08 02:42:33 +0000

    update

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 module.exports = {
     consumes: ["app", "console", "window"],
-    provides: ["onlykeyApi", "kbpgp", "forge", "nacl" , "pgpDecoder"],
+    provides: ["onlykeyApi", "kbpgp", "forge", "nacl" , "pgpDecoder", "onlykey3rd"],
     setup: function(options, imports, register) {
 
         imports.kbpgp = require('./onlykey/kbpgp-2.1.0.ok.js');
@@ -11,7 +11,102 @@ module.exports = {
         const onlykeyApi = require('./onlykey/onlykey-api.js')(imports);
         const onlykeyPGP = require('./onlykey/onlykey-pgp.js')(imports);
         const onlykey3rd = require('./onlykey/onlykey-3rd-party.js')(imports, onlykeyApi);
+        
+        
+        const request = require('superagent');
+        var getKey = function getKey(url, statusFn_force) {
+            var statusFn, force;
+            if (typeof statusFn_force == "string")
+                force = statusFn_force;
 
+            if (!url) return new Promise(resolve => { resolve(false) });
+
+            //pgp key 
+            if (url.slice(0, 10) == '-----BEGIN')
+                return new Promise(resolve => {
+                    if (statusFn) statusFn('Loaded public key (input) ...');
+                    resolve(url);
+                });
+
+            if (force) {
+                switch (force) {
+                    case 'protonmail':
+                        return protonmail();
+                    case 'keybase':
+                        return protonmail();
+                    case 'secure':
+                        return secure();
+                    default:
+                        break;
+                }
+            }
+
+
+            if (url.slice(0, 8) == 'https://') return secure();
+            if (!(url.indexOf("@") == -1)) return protonmail();
+            return keybase();
+
+            //direct url
+            function secure() {
+                return new Promise(resolve => {
+                    if (statusFn) statusFn('Downloading public key (https-url) ...');
+                    request
+                        .get(url)
+                        .end((err, key) => {
+                            if (err) {
+                                resolve(false);
+                                //err.message += ' Try to directly paste the public PGP key in.';
+                                //this.showError(err);
+                                return;
+                            }
+                            resolve(key.text);
+                            return key.text;
+                        });
+                });
+            }
+            //protonmail 
+            function protonmail() {
+                return new Promise(resolve => {
+                    if (statusFn) statusFn('Downloading public key (protonmail) ...');
+                    url = 'https://onlykey.herokuapp.com/protonmail/get/' + url;
+                    request
+                        .get(url)
+                        .set("Content-Type", "text/plain")
+                        .end((err, key) => {
+                            if (err) {
+                                resolve(false);
+                                //err.message += ' Try to directly paste the public PGP key in.';
+                                //this.showError(err);
+                                return;
+                            }
+                            resolve(key.text);
+                            return key.text;
+                        });
+                });
+            }
+            //keybase  or url
+            function keybase() {
+                return new Promise(resolve => {
+                    //button.textContent = 'Downloading public key ...';
+                    if (statusFn) statusFn('Downloading public key (keybase) ...');
+                    url = 'https://keybase.io/'.concat(url, '/pgp_keys.asc');
+                    request
+                        .get(url)
+                        .end((err, key) => {
+                            if (err) {
+                                resolve(false);
+                                //err.message += ' Try to directly paste the public PGP key in.';
+                                //this.showError(err);
+                                return;
+                            }
+                            resolve(key.text);
+                            return key.text;
+                        });
+                });
+            }
+        };
+        onlykeyApi.getKey = getKey;
+        
         register(null, {
             onlykeyApi: {
                 api: onlykeyApi,
@@ -20,6 +115,7 @@ module.exports = {
                 },
                 onlykey3rd : onlykey3rd,
             },
+            onlykey3rd : onlykey3rd,
             kbpgp:  imports.kbpgp(false,imports.console),
             forge: imports.nacl,
             nacl: imports.forge,
